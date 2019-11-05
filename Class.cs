55e98d887f41303a3ab7e6394201312743c1f961
alexandervrs <alexander@vourtsis.com>
2019-11-05 20:30:32 +0200

    Update Class.cs

--- Class.cs
+++ Class.cs
@@ -38,6 +38,7 @@ namespace MyNamespace.MyUtils {
 }
 // -------------( MyClass.cs )--------------
 
+
 /* Step 2: create your script */
 
 // using
@@ -55,7 +56,7 @@ classObj.IsPlaying = false;  // sets the playing state of that object
 ----------------------------------------- */
 
 /* Step 1: create a static class */
-
+// -------------( ListExtensions.cs )--------------
 public static class ListExtensions
 {
 
@@ -90,6 +91,7 @@ public static class ListExtensions
 	}
 
 }
+// -------------( ListExtensions.cs )--------------
 
 /* Step 2: use the new methods */
 
@@ -115,6 +117,7 @@ public class MyClassObjectContents
 
 
 /* Step 2: create a class for the chaining methods */
+// -------------( MyClass.cs )--------------
 public class MyClass {
 
     MyClassObjectContents item = new MyClassObjectContents();
@@ -148,7 +151,285 @@ public class MyClass {
     }
 
 }
+// -------------( MyClass.cs )--------------
 
 /* Step 3: use the new methods */
+
 MyClass myClass = new MyClass();
 myClass.SetName("somename").SetDescription("somedesc"); // chain the methods together
+
+
+/* -----------------------------------------
+   Inherit from a Base Class
+----------------------------------------- */
+
+/* Step 1: create a Base parent class */
+// -------------( BaseClass.cs )--------------
+public abstract class BaseClass // parent class is marked as abstract so we can define abstract/virtual methods
+{
+    
+    // variable that only exists in BaseClass but can be get/set from its child classes
+    // protected means private but allows its derived classes to access it
+    public virtual string classVariable { get; set; }
+
+    // this variable is ONLY access in this class, not accessible from ChildClass
+    private string classOnlyVariable = "classOnlyVariable";
+
+    // abstract method exists conceptually but lacks an implementation
+    // implementation should be done in ChildClass
+    public abstract string AbstractMethod();
+
+    // method with an implementation that can be overriden in ChildClass
+    public virtual string VirtualMethod()
+    {
+        return "BaseClass VirtualMethod()";
+    }
+
+    // regular method in BaseClass, cannot be overriden
+    // can be accessed with base.RegularMethodGetVariableFromBase() from ChildClass
+    public string RegularMethodGetVariableFromBase()
+    {
+        return "RegularMethodGetVariableFromBase()";
+    }
+
+}
+// -------------( BaseClass.cs )--------------
+
+
+/* Step 2: create a class that would inherit from Base class */
+// -------------( ChildClass.cs )--------------
+public class ChildClass : BaseClass // child of and inherits from BaseClass
+{
+
+    // variable that only exists in ChildClass
+    public string classVariable = "classVariable from Child Class";
+
+    // implementation of AbstractMethod()
+    public override string AbstractMethod()
+    {
+        return "ChildClass AbstractMethod()";
+    }
+
+    // overrides BaseClass VirtualMethod() with this implementation instead
+    public override string VirtualMethod()
+    {
+        // base.VirtualMethod(); // you can also call the same VirtualMethod() from BaseClass inside the same method
+        // OR
+        // return base.VirtualMethod(); // you can also return the result of VirtualMethod() instead
+        // OR
+        return "ChildClass VirtualMethod()"; // return the result from this VirtualMethod() in ChildClass
+    }
+
+    // return VirtualMethod() from BaseClass from this method
+    public string CallBaseVirtualMethod()
+    {
+        return base.VirtualMethod();
+    }
+
+    // returns classVariable from this ChildClass
+    public string GetChildClassVariable()
+    {
+        return this.classVariable;
+    }
+
+    // returns classVariable from BaseClass
+    public string GetBaseClassVariable()
+    {
+        return base.classVariable;
+    }
+
+	// calls RegularMethodGetVariableFromBase() from BaseClass
+    public string CallRegularMethodGetVariableFromBase()
+    {
+        return base.RegularMethodGetVariableFromBase();
+    }
+
+}
+// -------------( ChildClass.cs )--------------
+
+/* Step 3: use the methods */
+
+// use class that implements a base class
+ChildClass obj = new ChildClass();
+
+Console.WriteLine( obj.VirtualMethod() );
+
+
+/* -----------------------------------------
+   Implement from Interface Class
+----------------------------------------- */
+
+/* Step 1: create an interface class */
+// -------------( IClass.cs )--------------
+// define an interface, usually interface names have an "I" prefix (.NET guidelines suggest it does)
+public interface IClass 
+{
+
+    // note: all properties and methods in an interface are public
+	//       interfaces do not provide the implementation 
+	//       but rather ensure the class that implements them does implement all their functionality
+
+    // add a property to our interface
+    int InterfaceProperty { get; set; }
+
+    // add a method to our interface that will return a string result
+    string InterfaceMethod();
+
+}
+// -------------( IClass.cs )--------------
+
+
+/* Step 2: create a second class that will implement that interface */
+
+// -------------( ClassThatImplementsInterface.cs )--------------
+public class ClassThatImplementsInterface : IClass // implements interface IClass, you can implement multiple interfaces like "ClassThatImplementsInterface : IClass, IDisposable, IEquatable" etc.
+{
+
+    // implementation of InterfaceProperty from IClass in this class
+    public int InterfaceProperty { get; set; }
+
+    // implementation of InterfaceMethod() from IClass in this class
+    public string InterfaceMethod()
+    {
+        return "ClassThatImplementsInterface InterfaceMethod()";
+    }
+
+}
+// -------------( ClassThatImplementsInterface.cs )--------------
+
+/* Step 3: use the methods */
+
+// use class that implements interface
+ClassThatImplementsInterface obj = new ClassThatImplementsInterface();
+
+Console.WriteLine( obj.InterfaceMethod() );
+
+
+/* -----------------------------------------
+   Split Class into multiple files
+----------------------------------------- */
+
+// note: partial keyword is used to split class files into multiple ones
+//       useful to reduce noise with classes or when a class has a section
+//       that doesn't need to be changed as often
+
+/* Step 1: create the class files */
+// -------------( PartialClassA.cs )--------------
+public partial class PartialClass // PartialClass A
+{
+
+    public int PartialProperty { set; get; }
+
+    public string Method1()
+    {
+        return "Method1() in PartialClassA.cs";
+    }
+
+    // Method2() is implemented in PartialClassB...
+}
+// -------------( PartialClassA.cs )--------------
+
+// -------------( PartialClassB.cs )--------------
+public partial class PartialClass // PartialClass B
+{
+
+    // Method1() is implemented in PartialClassA...
+    
+    public string Method2()
+    {
+        this.PartialProperty = 3;
+        return "Method2() in PartialClassB.cs, PartialProperty is "+this.PartialProperty.ToString();
+    }
+
+}
+// -------------( PartialClassB.cs )--------------
+
+/* Step 3: use the methods */
+
+PartialClass classObj = new PartialClass();
+Debug.Log(classObj.Method2());
+
+
+/* -----------------------------------------
+   Normal classes
+----------------------------------------- */
+
+// note: a class can be instantiated with the "new" keyword
+
+/* Step 1: create your class file */
+
+// -------------( MyClass.cs )--------------
+public class MyClass
+{
+    // constructor, use the same name as the class, gets called when a Class object has been created
+    public SealedClass()
+    {
+        UnityEngine.Debug.Log("Constructor()");
+    }
+
+    // destructor gets called when a Class object has been discarded
+    ~SealedClass()
+    {
+        UnityEngine.Debug.Log("~Destructor()");
+    }
+
+    // a method of this class
+    public string Method()
+    {
+        return "Method in MyClass";
+    }
+}
+// -------------( MyClass.cs )--------------
+
+/* Step 2: use the methods */
+
+MyClass classObj = new MyClass(); // create a new object of MyClass
+Console.WriteLine(classObj.Method());
+
+
+/* -----------------------------------------
+   Static classes
+----------------------------------------- */
+
+// note: a class marked as static cannot be instantiated with the "new" keyword, provides a performance boost
+
+/* Step 1: create your class file */
+
+// -------------( MyStaticClass.cs )--------------
+public static class MyStaticClass
+{
+    public static string Method()
+    {
+        return "Method in MyStaticClass";
+    }
+}
+// -------------( MyStaticClass.cs )--------------
+
+/* Step 2: use the methods */
+
+Console.WriteLine(MyStaticClass.Method()); // call a static method from the MyStaticClass, no need for "new" keyword
+
+
+/* -----------------------------------------
+   Sealed classes
+----------------------------------------- */
+
+// note: a class marked as sealed cannot be inherited from but can be instantiated with "new" keyword, sometimes it provides a performance boost
+
+/* Step 1: create your class file */
+
+// -------------( MySealedClass.cs )--------------
+public sealed class MySealedClass
+{
+    public string Method()
+    {
+        return "Method in MySealedClass";
+    }
+}
+// -------------( MySealedClass.cs )--------------
+
+/* Step 2: use the methods */
+
+MySealedClass classObj = new MySealedClass(); // create a new object of MySealedClass
+Console.WriteLine(classObj.Method());
+
