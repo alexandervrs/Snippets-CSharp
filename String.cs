791ccd8bf1caa4346c1173cc4c314ca8850445ad
alexandervrs <alexander@vourtsis.com>
2019-11-07 21:34:37 +0200

    Update String.cs

--- String.cs
+++ String.cs
@@ -14,7 +14,7 @@ using System.Text.RegularExpressions; // for Regex
    Text operations
 ----------------------------------------- */
 // create a new string
-string text = "Testing,123";
+string textString = "Testing,123";
 
 // create a new string array
 string[] textArray = new string[] {"Text1", "Text2", "Text3"};
@@ -22,25 +22,29 @@ string[] textArray = new string[] {"Text1", "Text2", "Text3"};
 // create a new string list
 List<string> textList = new List<string>(){"TextA", "TextB", "TextC"};
 
+// interpolate strings with $, string interpolation does not create new string objects like "firstString"+","+"secondString" would do
+string firstString = "first"; string secondString = "second";
+string mergedString = $"{firstString}, {secondString}";
+
 // keep substring text between start position (1) and last position (3)
-string textPart = text.Substring(1, 3); // returns "est"
+string textPart = textString.Substring(1, 3); // returns "est"
 
 // returns character from string at position (4) as a string
-text[4].ToString();
+textString[4].ToString();
 
 // repeat a string (12) times
-string.Join(text, new string[12 + 1]);
+string.Join(textString, new string[12 + 1]);
 
 // mask a string with a character (*)
-string maskedString = string.Join("*", new string[text.Length + 1]);
+string maskedString = string.Join("*", new string[textString.Length + 1]);
 
 // reverse a string
-char[] inArray = text.ToCharArray();
+char[] inArray = textString.ToCharArray();
 Array.Reverse(inArray);
 string reversedString = new string(inArray);
 
 // shuffle a string's characters
-char[] inArray = text.ToCharArray();
+char[] inArray = textString.ToCharArray();
 Random rng     = new Random();
 int    n       = inArray.Length;
 while (n > 1) {
@@ -50,7 +54,7 @@ while (n > 1) {
 	inArray[k] = inArray[n];
 	inArray[n] = value;
 }
-string reversedString = new string(inArray);
+string shuffledString = new string(inArray);
 
 // join a string array together with a delimiter ","
 string joinedText = string.Join(",", textArray);
@@ -59,41 +63,41 @@ string joinedText = string.Join(",", textArray);
 string joinedText = string.Join(",", textArray.ToArray());
 
 // split a string to a string array at a delimiter ","
-string[] stringParts = text.Split(",".ToCharArray()); // returns [Testing, 123]
+string[] stringParts = textString.Split(",".ToCharArray()); // returns [Testing, 123]
 
 // replace all occurences in a string
-string replacedString = Regex.Replace(text, "Testing", "replaced"); // text, replaceThis, replaceWith | returns "replaced,123"
+string replacedString = Regex.Replace(textString, "Testing", "replaced"); // text, replaceThis, replaceWith | returns "replaced,123"
 
 // check if string starts with substring "T"
-bool startsWith = text.StartsWith("T"); // true
+bool startsWith = textString.StartsWith("T"); // true
 
 // check if string ends with substring "T"
-bool endsWith = text.EndsWith("w"); // false
+bool endsWith = textString.EndsWith("w"); // false
 
 // trim string left using characterSet (checks and removes T from the left side)
 string characterSet = "T";
-string trimmedString = Regex.Replace(text, "^"+characterSet+"+", "");
+string trimmedString = Regex.Replace(textString, "^"+characterSet+"+", "");
 
 // trim string right using characterSet (checks and removes 3 from the right side)
 string characterSet = "3";
-string trimmedString = Regex.Replace(text, ""+characterSet+"+$", "");
+string trimmedString = Regex.Replace(textString, ""+characterSet+"+$", "");
 
 // trim string using characterSet (checks and removes T from both sides)
 string characterSet = "T";
-string trimmedString = Regex.Replace(text, "^"+characterSet+"+|"+characterSet+"+$", "");
+string trimmedString = Regex.Replace(textString, "^"+characterSet+"+|"+characterSet+"+$", "");
 
 // convert text to uppercase
-text.ToUpper();
+textString.ToUpper();
 
 // convert text to lowercase
-text.ToLower();
+textString.ToLower();
 
 // returns a string containing the character with the given Unicode code (Chr())
 int code = 84;
 string character = (Convert.ToChar( code )).ToString(); // returns "T"
 
 // returns the Unicode value code of the first character in the given string (Ord())
-string outString = ((int)text[0]).ToString();
+string outString = ((int)textString[0]).ToString();
 int code = System.Convert.ToInt32(outString); // code will be 84 since "T" is the first letter in text
 
 // generate a new UUIDv4
@@ -104,6 +108,6 @@ byte[] data = Convert.FromBase64String("VGVzdGluZywxMjM=");
 string decodedString = Encoding.UTF8.GetString(data); // returns "Testing,123"
 
 // encode to a base64 string
-byte[] data = Encoding.UTF8.GetBytes(text);
+byte[] data = Encoding.UTF8.GetBytes(textString);
 return Convert.ToBase64String(data).ToString(); // returns "VGVzdGluZywxMjM="
 
