faabbc04f6cb5759c05a8471eaa56908ad1847a4
alexandervrs <alexander@vourtsis.com>
2019-12-21 21:36:25 +0200

    Update UI.SplitContainer.cs

--- UI.SplitContainer.cs
+++ UI.SplitContainer.cs
@@ -4,7 +4,7 @@
  * SplitContainer related snippets for C#
  */
 
-using System.Windows.Forms; // (Windows-Only) for Control, Form
+using System.Windows.Forms; // (Windows-Only) for Control, Form, SplitContainer, Orientation
 
 
 /* -----------------------------------------
@@ -20,6 +20,7 @@ splitcontainer1.Size          = new Size(200, 140);
 splitcontainer1.Dock          = DockStyle.Fill;
 splitcontainer1.BorderStyle   = BorderStyle.FixedSingle;
 splitcontainer1.SplitterWidth = 5; // width of Splitter grip
+splitContainer2.Orientation   = Orientation.Horizontal; // Horizontal or Vertical
 
 splitcontainer1.Panel1Collapsed = false; // set panel1 to visible
 splitcontainer1.Panel2Collapsed = false; // set panel2 to visible
